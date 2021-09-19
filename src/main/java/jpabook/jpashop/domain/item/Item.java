cd866cee3b68934b92939be1a9fa1abcef554aa5
jifrozen <answldjs1836@naver.com>
2021-09-20 02:24:17 +0900

    [Feat] Item domain development

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -13,6 +13,7 @@ import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -34,5 +35,18 @@ public abstract class Item {
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories=new ArrayList<>();
 
+
+	// 비즈니스 로직 -> entity 안에서 ( 응집력 )
+	public void addStock(int quantity){
+		this.stockQuantity+=quantity;
+	}
+
+	public void removeStock(int quantity){
+		int restStock=this.stockQuantity-quantity;
+		if (restStock<0){
+			throw new NotEnoughStockException("need more stock");
+		}
+		this.stockQuantity=restStock;
+	}
 
 }
