ac0c8921865d65605a6039694d112e89b667c404
jifrozen <answldjs1836@naver.com>
2021-09-22 00:20:01 +0900

    [Feat] 주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -2,10 +2,14 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.Embeddable;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Embeddable
 @Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Address {
 
 	private String city;
